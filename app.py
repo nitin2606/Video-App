f1fc5dbef2d0051f51433cea37f3d79024939bf6
nitin2606 <nitinmaurya2606@gmail.com>
2023-12-29 14:09:20 +0530

    new updates

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
 from flask import Flask, render_template
-#import cloudinary
-#import cloudinary.uploader
-#import cloudinary.api
 
 app = Flask(__name__)
 
-# Configure Cloudinary with your cloud_name, api_key, and api_secret
-'''cloudinary.config(
-    cloud_name="your_cloud_name",
-    api_key="your_api_key",
-    api_secret="your_api_secret"
-)'''
-
-# Replace 'video_urls' with the actual video URLs from Cloudinary
-video_urls = [
-    "https://res.cloudinary.com/dam12ojlp/video/upload/v1703835359/reels/vid1_eq0z7e.mp4"
-
-    # Add more video URLs as needed
-]
+# Replace 'video_url' with the actual video URL from Cloudinary
+video_url = "https://res.cloudinary.com/dam12ojlp/video/upload/v1703835359/reels/vid1_eq0z7e.mp4"
 
 @app.route('/')
 def index():
-    return render_template('index.html', video_urls=video_urls)
+    return render_template('index.html', video_url=video_url)
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run()
+    app.run(debug=True)
---
+++ app_scroll.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+#import cloudinary
+#import cloudinary.uploader
+#import cloudinary.api
+
+app = Flask(__name__)
+
+# Configure Cloudinary with your cloud_name, api_key, and api_secret
+'''cloudinary.config(
+    cloud_name="your_cloud_name",
+    api_key="your_api_key",
+    api_secret="your_api_secret"
+)'''
+
+# Replace 'video_urls' with the actual video URLs from Cloudinary
+video_urls = [
+    "https://res.cloudinary.com/dam12ojlp/video/upload/v1703835359/reels/vid1_eq0z7e.mp4"
+
+    # Add more video URLs as needed
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', video_urls=video_urls)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    #app.run()
